f90644eb582dd0263b4fa6ecbdabc5b85b55184d
punnapaht <52537071+punnapaht@users.noreply.github.com>
2019-07-05 23:26:58 +0700

    Update Exercise4_Punnapaht_I.py

--- Exercise4_Punnapaht_I.py
+++ Exercise4_Punnapaht_I.py
@@ -3,7 +3,7 @@ GB = 85
 IC = 60
 CP = 89.5
 
--------Your Score-------
+----Your Score----
 print("Foundation English :",FE ,"Points")
 print("General Business :",GB ,"Points")
 print("Introduction to Computer System :",IC ,"Points")
