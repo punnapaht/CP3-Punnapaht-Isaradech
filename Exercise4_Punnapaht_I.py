3a6679894f7ce8f18f4eb70cee86d909cea2e3c1
punnapaht <52537071+punnapaht@users.noreply.github.com>
2019-07-05 23:26:37 +0700

    Update and rename README.md to Exercise4_Punnapaht_I.py

---
+++ Exercise4_Punnapaht_I.py
@@ -0,0 +1,10 @@
+FE = 90
+GB = 85
+IC = 60
+CP = 89.5
+
+-------Your Score-------
+print("Foundation English :",FE ,"Points")
+print("General Business :",GB ,"Points")
+print("Introduction to Computer System :",IC ,"Points")
+print("Computer Programming :",CP ,"Points")
