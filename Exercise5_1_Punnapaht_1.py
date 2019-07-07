5c44c6e2b9160eb0745d7027392b29bbbef98f85
punnapaht <52537071+punnapaht@users.noreply.github.com>
2019-07-07 23:52:09 +0700

    Create Exercise5_1_Punnapaht_1.py

---
+++ Exercise5_1_Punnapaht_1.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter First Number :"))
+number2 = int(input("Enter Second Number :"))
+
+
+print("Calculate :",number1,"+",number2,"=",number1+number2)
+print("Calculate :",number1,"x",number2,"=",number1*number2)
+print("Calculate :",number1,"/",number2,"=",number1/number2)
+print("Calculate :",number1,"-",number2,"=",number1-number2)
